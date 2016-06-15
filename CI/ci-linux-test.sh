456de93bc90a8656f49a46313b8814c646341491
Jan Möbius <moebius@cs.rwth-aachen.de>
2016-06-15 10:18:43 +0200

    Added seperate test stage

---
+++ CI/ci-linux-test.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+COMPILER=$1
+LANGUAGE=$2
+
+# Exit script on any error
+set -e 
+
+source linux-settings.sh
+
+echo -e "${OUTPUT}"
+echo ""
+echo "======================================================================"
+echo "Running unittests Release version with vectorchecks enabled"
+echo "======================================================================"
+echo -e "${NC}"
+
+if [ ! -d build-release-$BUILDPATH-Vector-Checks ]; then
+  echo "Build Artifacts not found in: build-release-$BUILDPATH-Vector-Checks"
+  exit 1
+fi
+
+cd build-release-$BUILDPATH-Vector-Checks
+
+cd Unittests
+
+#execute tests
+./unittests --gtest_color=yes --gtest_output=xml
+
+cd ..
+cd ..
+
+
+echo -e "${OUTPUT}"
+echo ""
+echo "======================================================================"
+echo "Running Python unittests Release version "
+echo "======================================================================"
+echo -e "${NC}"
+
+if [ ! -d build-release-$BUILDPATH ]; then
+  echo "Build Artifacts not found in: build-release-$BUILDPATH"
+  exit 1
+fi
+
+cd build-release-$BUILDPATH
+
+if [ "$LANGUAGE" == "C++11"  ] || [ "$COMPILER" == "gcc" ] ; then
+
+  # Execute Python unittests
+  cd Python-Unittests
+
+  python -m unittest discover -v
+
+  cd ..
+
+else
+  echo -e "${WARNING}"
+  echo "WARNING! Python unittests disabled !!"
+  echo -e "${NC}"
+fi
+
+cd ..
+
+
+echo -e "${OUTPUT}"
+echo ""
+echo "======================================================================"
+echo "Running unittests Debug version with vectorchecks enabled"
+echo "======================================================================"
+echo -e "${NC}"
+
+
+if [ ! -d build-debug-$BUILDPATH-Vector-Checks ]; then
+  echo "Build Artifacts not found in: build-debug-$BUILDPATH-Vector-Checks"
+  exit 1
+fi
+
+cd build-debug-$BUILDPATH-Vector-Checks
+cd Unittests
+
+#execute tests
+./unittests --gtest_color=yes --gtest_output=xml
+
+cd ..
+cd ..
+
+
+
+echo -e "${OUTPUT}"
+echo ""
+echo "======================================================================"
+echo "Running Python unittests Debug version "
+echo "======================================================================"
+echo -e "${NC}"
+
+if [ ! -d build-debug-$BUILDPATH ]; then
+  echo "Build Artifacts not found in: build-debug-$BUILDPATH"
+  exit 1
+fi
+
+cd build-debug-$BUILDPATH
+
+if [ "$LANGUAGE" == "C++11"  ] || [ "$COMPILER" == "gcc" ] ; then
+
+  # Execute Python unittests
+  cd Python-Unittests
+
+  python -m unittest discover -v
+else
+
+  echo -e "${WARNING}"
+  echo "WARNING! Python unittests disabled !!"
+  echo -e "${NC}"
+
+fi
